09d386f9115e1c1bf882fafdab20dc0e2e2d3677
muhammaddzikra <muhammaddzikra280311@gmail.com>
2024-09-04 09:44:35 +0700

    ganti motto,nim,funfact

--- Halaman Utama/halaman_utama.py
+++ Halaman Utama/halaman_utama.py
@@ -159,9 +159,9 @@ elif menu == "About Us":
             {
                 "nama": "Muhammad Dzikra",
                 "sebagai": "Anggota",
-                "nim": "122450124",
-                "fun_fact": "Suka Fadil",
-                "motto_hidup": "saya ganteng",
+                "nim": "123450124",
+                "fun_fact": "ga punya suku",
+                "motto_hidup": "yang penting hidup",
             },
             {
                 "nama": "D",
